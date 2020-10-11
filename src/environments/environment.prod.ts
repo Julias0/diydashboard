281902e659f7988bf9ef10c29e000feb49cf5d5f
aiyush <aiyush.dhar@fyle.in>
2020-10-11 10:35:02 +0530

    Updated PR

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: 'AIzaSyBb5-Ny8jwmvYbFwjzyPxw_oI4O2I-52aY',
+        authDomain: 'diydashboard-cac66.firebaseapp.com',
+        databaseURL: 'https://diydashboard-cac66.firebaseio.com',
+        projectId: 'diydashboard-cac66',
+        storageBucket: 'diydashboard-cac66.appspot.com',
+        messagingSenderId: '129067084505',
+        appId: '1:129067084505:web:d03552c752d26ba7c32cc8',
+        measurementId: 'G-XNBJ9DP0MK'
+    }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+  // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
